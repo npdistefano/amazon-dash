0659c9e5135da3a85a2c409c946df70f54b68dc9
Nick Di Stefano <npdistefano@users.noreply.github.com>
2018-01-06 22:28:11 -0500

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+
+MAC_ADDRESS = 'B47C9CE547C7' # Amazon Charmin Dash Button MAC Address
+
+def detect_button(pkt):
+    if pkt.haslayer(DHCP) and pkt[Ether].src == MAC_ADDRESS:
+            print "Button Press Detected" 
+            #Do stuff
+
+sniff(prn=detect_button, filter="(udp and (port 67 or 68))", store=0)
