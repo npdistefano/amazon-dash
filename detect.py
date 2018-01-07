2f054e578803f0330121e103e385b99165a9dc4b
Nick Di Stefano <npdistefano@gmail.com>
2018-01-07 14:37:24 -0500

    MAC address update
    MAC address update

--- detect.py
+++ detect.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 from scapy.all import *
 
-MAC_ADDRESS = 'B47C9CE547C7' # Amazon Charmin Dash Button MAC Address
+MAC_ADDRESS = 'B4:7C:9C:E5:47:C7' # Amazon Charmin Dash Button MAC Address
 
 def detect_button(pkt):
     if pkt.haslayer(DHCP) and pkt[Ether].src == MAC_ADDRESS:
